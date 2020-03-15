3e57de31749375f0c90e4b2a7366b2d843dc6158
Ryan Seit <ryan@ryanseit.com>
2020-03-14 20:45:27 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,28 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student = Student.find params[:id]
+    if @student.active == false
+      @student.update_attribute(:active, true)
+    else
+      @student.update_attribute(:active, false)
+    end
+    @student.save
+    redirect_to "/students/#{@student.id}"
+  end
+
+  # def active
+  #   @student = Student.find params[:id]
+  #   if @student.active == false
+  #     @student.update_attribute(:active, true)
+  #   else
+  #     @student.update_attribute(:active, false)
+  #   end
+  #   @student.save
+  #   redirect_to @show
+  # end
+
   private
 
     def set_student
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:index, :show]
+  resources :students do
+    member do
+      get :activate
+      patch :activate
+      put :activate
+    end
+  end
+
 end
